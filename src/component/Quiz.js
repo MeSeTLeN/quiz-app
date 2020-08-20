b206f2ce9a278fb8fd47ad57ff7f96e2930d8e52
DeAdFrOnt <semenukmaks@gmail.com>
2020-08-20 11:46:13 +0300

    add: assign state

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import QuizData from '../Data/QuizData'
+import React, { Component } from "react";
+import QuizData from "../Data/QuizData";
 
 export class Quiz extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+
+  // Initializing local state by assigning an object to this.state. by using constructor
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userAnswer: null,
+      currentQuestion: 0,
+      options: [],
+      quizEnd: false, //true if last question
+      score: 0,
+      disableBtn: true, //disable btn after press
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
 }
 
-export default Quiz
+export default Quiz;
