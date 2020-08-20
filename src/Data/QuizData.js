dfc7145decae52642819fb7e4bbab31bb222de21
DeAdFrOnt <semenukmaks@gmail.com>
2020-08-20 11:31:48 +0300

    init: quiz app layout

---
+++ src/Data/QuizData.js
@@ -0,0 +1,38 @@
+export const QuizData = [
+    {
+        id:0,
+        question: 'What number here 1 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+    {
+        id:1,
+        question: 'What number here 2 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+    {
+        id:2,
+        question: 'What number here 3 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+    {
+        id:3,
+        question: 'What number here 4 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+    {
+        id:4,
+        question: 'What number here 5 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+    {
+        id:5,
+        question: 'What number here 6 ?',
+        options: ['0','2','4','5','1','6'],
+        answer:'1'
+    },
+]
