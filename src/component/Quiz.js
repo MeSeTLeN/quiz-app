06c3f052aad57c963d8ab9d9833b78e21c7b49aa
DeAdFrOnt <semenukmaks@gmail.com>
2020-08-20 12:05:03 +0300

    add: func to inc score, reset answer, inc index

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -15,12 +15,9 @@ export class Quiz extends Component {
     };
   }
 
-  //   specifi state in func
-
   loadQuiz = () => {
-    // selecting currentIndex
     const { currentIndex } = this.state;
-    // defining by setState what will return back
+
     this.setState(() => {
       return {
         question: QuizData[currentIndex].question,
@@ -30,6 +27,24 @@ export class Quiz extends Component {
     });
   };
 
+  //   new func it do increment currentIndex and if userAnswer correct increment score
+  nextQuestionHandler = () => {
+    // retrive value of those things
+    const { userAnswer, answer, score } = this.state;
+
+    // check correct answer and increase score if correct
+    if (userAnswer=== answer){
+        this.setState({
+            score: score + 1
+        })
+    }
+
+    // increse currentIndex by 1 and reset user answer
+    this.setState({
+        currentIndex: this.state.currentIndex + 1
+        userAnswer: null
+    })
+  };
   render() {
     return <div></div>;
   }
